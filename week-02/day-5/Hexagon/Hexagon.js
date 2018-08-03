d3c1fb93552ae371dfcd446ac7dd1b3b373ff987
YanqingYao <yaoyanqing@email.arizona.edu>
2018-08-03 12:45:07 +0800

    8/3

--- week-02/day-5/Hexagon/Hexagon.js
+++ week-02/day-5/Hexagon/Hexagon.js
@@ -1,21 +1,13 @@
-function triangle(num) {
-  for (let i = 0; i < num; i++) {
-    const w = 18;
-    const h = 24;
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 + w * i, i * h);
-    ctx.lineTo(canvas.width / 2 - (num - i * 2) * w, num * h);
-    ctx.stroke();
+'use strict';
 
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - w * i, i * h);
-    ctx.lineTo(canvas.width / 2 + (num - i * 2) * w, num * h);
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - (i + 1) * w, (i + 1) * h);
-    ctx.lineTo(canvas.width / 2 + (i + 1) * w, (i + 1) * h);
-    ctx.stroke();
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+function hexagon() {
+  ctx.beginPath();
+  ctx.moveTo(canvas.width / 2 + 30 * Math.cos(0), 15 * Math.sqrt(3) + 30 * Math.sin(0));
+  for (let i = 0; i < 7; i++) {
+    ctx.lineTo(canvas.width / 2 + 30 * Math.cos(i * 2 * Math.PI / 6), 15 * Math.sqrt(3) + 30 * Math.sin(i * 2 * Math.PI / 6));
   }
+  ctx.stroke();
 }
-triangle(10);
+hexagon(10);
--- week-02/day-5/Triangle/triangle.js
+++ week-02/day-5/Triangle/triangle.js
@@ -3,17 +3,24 @@
 const canvas = document.querySelector('.main-canvas');
 const ctx = canvas.getContext('2d');
 
-function triangular(value) {
-  const abs = Math.abs(value);
-  return ((abs / 2) * (abs + 1)) * (abs / value) || 0;
-}
-
 function triangle(num) {
-  ctx.beginPath();
-  ctx.moveTo(20 + 20 * Math.cos(0), 100 + 20 * Math.sin(0));
-  for (let i = 0; i < 7; i++) {
-    ctx.lineTo(20 + 20 * Math.cos(i * 2 * Math.PI / 6), 100 + 20 * Math.sin(i * 2 * Math.PI / 6));
+  for (let i = 0; i < num; i++) {
+    const w = 18;
+    const h = 24;
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 + w * i, i * h);
+    ctx.lineTo(canvas.width / 2 - (num - i * 2) * w, num * h);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 - w * i, i * h);
+    ctx.lineTo(canvas.width / 2 + (num - i * 2) * w, num * h);
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(canvas.width / 2 - (i + 1) * w, (i + 1) * h);
+    ctx.lineTo(canvas.width / 2 + (i + 1) * w, (i + 1) * h);
+    ctx.stroke();
   }
-  ctx.stroke();
 }
 triangle(10);
