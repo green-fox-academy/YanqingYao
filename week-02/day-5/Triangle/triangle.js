0708c30b64aeb153a152d5d8cced98912cede30f
YanqingYao <yaoyanqing@email.arizona.edu>
2018-08-06 14:25:21 +0800

    8/6

--- week-02/day-5/Triangle/triangle.js
+++ week-02/day-5/Triangle/triangle.js
@@ -7,15 +7,15 @@ function triangle(num) {
   for (let i = 0; i < num; i++) {
     const w = 18;
     const h = 24;
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 + w * i, i * h);
-    ctx.lineTo(canvas.width / 2 - (num - i * 2) * w, num * h);
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(canvas.width / 2 + w * i, i * h);
+    // ctx.lineTo(canvas.width / 2 - (num - i * 2) * w, num * h);
+    // ctx.stroke();
 
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - w * i, i * h);
-    ctx.lineTo(canvas.width / 2 + (num - i * 2) * w, num * h);
-    ctx.stroke();
+    // ctx.beginPath();
+    // ctx.moveTo(canvas.width / 2 - w * i, i * h);
+    // ctx.lineTo(canvas.width / 2 + (num - i * 2) * w, num * h);
+    // ctx.stroke();
 
     ctx.beginPath();
     ctx.moveTo(canvas.width / 2 - (i + 1) * w, (i + 1) * h);
