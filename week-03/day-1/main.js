964a54e1bab2ab741ef87da2b5078fdce7739fac
YanqingYao <yaoyanqing@email.arizona.edu>
2018-08-06 15:38:43 +0800

    8/6

---
+++ week-03/day-1/main.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const column1 = document.createElement('div');
+const bread = document.createElement('p');
+const milk = document.createElement('p');
+const orange = document.createElement('p');
+const tomato = document.createElement('p');
+bread.textContent = 'bread';
+milk.textContent = 'milk';
+orange.textContent = 'orange';
+tomato.textContent = 'tomato';
+
+const column2 = document.createElement('div');
+const up = document.createElement('button');
+const biger = document.createElement('button');
+const x = document.createElement('button');
+const down = document.cÎreateElement('button');
+up.textContent = 'up';
+bigger.textContent = '>';
+x.textContent = 'X';
+down.textContent = 'down';
+
+const column3 = document.createElement('div');
