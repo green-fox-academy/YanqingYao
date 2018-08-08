8d00a0d6cd5fdabfe36473aa188640a5c130a0b2
YanqingYao <yaoyanqing@email.arizona.edu>
2018-08-08 19:59:58 +0800

    Hexagon reproduce done

--- week-03/day-3/reproduce.js
+++ week-03/day-3/reproduce.js
@@ -7,15 +7,15 @@ const ctx = canvas.getContext('2d');
 const reproduce = (w, x, y, n) => {
   if (n > 0) {
     ctx.beginPath();
-    ctx.moveTo(x, y);
-    for (let i = 0; i < 7; i++) {
-      ctx.lineTo(canvas.width / 2 + 30 * Math.cos(i * 2 * Math.PI / 6), 15 * Math.sqrt(3) + 30 * Math.sin(i * 2 * Math.PI / 6));
+    ctx.moveTo(x + w * Math.cos(0), y + w * Math.sin(0));
+    for (let side = 1; side < 7; side++) {
+      ctx.lineTo(x + w * Math.cos(side * 2 * Math.PI / 6), y + w * Math.sin(side * 2 * Math.PI / 6));
     }
     ctx.stroke();
+    reproduce(w / 2, w / 2 + x, y, n - 1);
+    reproduce(w / 2, x - w / 4, y - w / 4 * Math.sqrt(3), n - 1);
+    reproduce(w / 2, x - w / 4, y + w / 4 * Math.sqrt(3), n - 1);
   }
 };
 
-reproduce(300, canvas.width / 2, canvas.height / 2, 4);
-
-
-ctx.stroke();
+reproduce(200, canvas.width / 2, canvas.height / 2, 5);
