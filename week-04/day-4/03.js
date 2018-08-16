f75b8d27e845cc023c4420d5643169b51306b4ca
YanqingYao <yaoyanqing@email.arizona.edu>
2018-08-16 13:21:23 +0800

    greenfox done

--- week-04/day-4/03.js
+++ week-04/day-4/03.js
@@ -26,7 +26,7 @@ class Student extends Person {
   }
 
   introduce() {
-    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} from ${this.previousOrganization} who skipped ${skippedDays} days from the course already.`);
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days from the course already.`);
   }
 
   skipDays(numberOfDays) {
@@ -45,21 +45,94 @@ class Mentor extends Person {
   }
 
   introduce() {
-    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} level mentor.`);
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} ${this.level} mentor.`);
   }
 }
 
 class Sponsor extends Person {
-  constructor(name = 'Jane Doe', age = '30', gender = 'female', company = 'intermediate') {
+  constructor(name = 'Jane Doe', age = '30', gender = 'female', company = 'Google') {
     super(name, age, gender);
-    this.level = level;
+    this.company = company;
+    this.hiredStudents = 0;
   }
 
   getGoal() {  //eslint-disable-line
-    console.log('My goal is: Educate brilliant junior software developers.');
+    console.log('My goal is: Hire brilliant junior software developers.');
   }
 
   introduce() {
-    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} level mentor.`);
+    console.log(`Hi, I'm ${this.name}, a ${this.age} year old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far.`);
+  }
+
+  hire() {
+    this.hiredStudents += 1;
   }
 }
+
+class Cohort {
+  constructor(name) {
+    this.name = name;
+    this.students = [];
+    this.mentors = [];
+  }
+
+  addStudent(student) {
+    this.students.push(student);
+  }
+
+  addMentor(mentor) {
+    this.mentors.push(mentor);
+  }
+
+  info() {
+    console.log(`The ${this.name} cohort has ${this.students.length} students and ${this.mentors.length} mentors.`);
+  }
+}
+
+const people = [];
+
+const mark = new Person('Mark', 46, 'male');
+people.push(mark);
+
+const jane = new Person();
+people.push(jane);
+
+const john = new Student('John Doe', 20, 'male', 'BME');
+people.push(john);
+
+const student = new Student();
+people.push(student);
+
+const gandhi = new Mentor('Gandhi', 148, 'male', 'senior');
+people.push(gandhi);
+
+const mentor = new Mentor();
+people.push(mentor);
+
+const elon = new Sponsor('Elon Musk', 46, 'male', 'SpaceX');
+people.push(elon);
+
+const sponsor = new Sponsor();
+people.push(sponsor);
+
+student.skipDays(3);
+
+for (let i = 0; i < 6; i++) {
+  elon.hire();
+}
+
+for (let i = 0; i < 4; i++) {
+  sponsor.hire();
+}
+
+for (let person of people) {
+  person.introduce();
+  person.getGoal();
+}
+
+const awesome = new Cohort('AWESOME');
+awesome.addStudent(student);
+awesome.addStudent(john);
+awesome.addMentor(mentor);
+awesome.addMentor(gandhi);
+awesome.info();
