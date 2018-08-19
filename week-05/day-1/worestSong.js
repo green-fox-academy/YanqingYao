398ad9b4061df583b1f953360b8a02578d26d1c0
YanqingYao <yaoyanqing@email.arizona.edu>
2018-08-19 18:46:52 +0800

    worest song done

---
+++ week-05/day-1/worestSong.js
@@ -0,0 +1,34 @@
+
+const csvToJson = require('convert-csv-to-json');
+
+const worstSongs = csvToJson.getJsonFromCsv('100worst.csv');
+// console.log(worstSongs[1].year);
+
+function countSong(param) {
+  let count = 0;
+  let countMax = 1;
+  let songYear = [];
+  for (let i = 0; i < param.length; i++) {
+    for (let j = 0; j < param.length; j++) {
+      if (param[i].year === param[j].year) {
+        count += 1;
+      }
+      if (count === countMax) {
+        if (!songYear.includes(param[i].year)) {
+          songYear += ` & ${param[i].year}`;
+        }
+      }
+      if (count > countMax) {
+        countMax = count;
+        songYear = param[i].year;
+      }
+    }
+    count = 0;
+  }
+  return `The year when the most worst songs came out is${songYear}`;
+}
+
+console.log(countSong(worstSongs));
+
+// expected output
+// `The year when the most worst songs came out is <year>.`
