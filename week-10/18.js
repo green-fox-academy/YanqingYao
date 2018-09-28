2ab44d03c39982987fd90171b0acf037d5ca97c0
YanqingYao <yaoyanqing@email.arizona.edu>
2018-09-28 14:44:41 +0800

    exam done

--- week-10/18.js
+++ week-10/18.js
@@ -1,6 +1,6 @@
 const a = 'doge';
 const b = 'home';
 
-const findMatch = new RegExp(`[ ${b} +]`, 'g');
+const findMatch = new RegExp(`[ ${b} ]`, 'g');
 
 console.log(a.match(findMatch));
--- week-10/9.js
+++ week-10/9.js
@@ -1,5 +1,4 @@
 const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
-const findBiggest = (arr) => { Math.max(...arr.filter((item, i) => i % 2 !== 0)); };
-
+const findBiggest = arr => Math.max(...arr.filter((item, i) => i % 2 !== 0));
 
 console.log(findBiggest(array));
