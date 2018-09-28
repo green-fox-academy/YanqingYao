74f9fe0f5c473fa5644c42f1e7c6f600b6376d11
YanqingYao <yaoyanqing@email.arizona.edu>
2018-09-28 10:11:07 +0800

    exam done

---
+++ week-10/9.js
@@ -0,0 +1,12 @@
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+function findBiggest(arr) {
+  const newArr = [];
+  arr.forEach((i) => {
+    if (i % 2 !== 0) {
+      newArr.push(arr[i]);
+    }
+  });
+  return Math.max(...newArr);
+}
+
+console.log(findBiggest(array));
